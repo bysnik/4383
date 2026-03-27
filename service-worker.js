4bad2016c7cf2734d2c006f55c244ef9f4ca4213
Nikita Bystrov <bystrovno@basealt.ru>
2026-03-27 14:47:48 +0300

    Update web build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'testproject';
+var cacheName = '4383';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
